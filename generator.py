267581f454830146a75e87fc1c9058f33707db22
Libin Mathew <loginlive10@gmail.com>
2022-08-27 13:03:49 +0530

    Delete and View
    Deleted debug codes and added nb cell to view output as pandas dataFrame

--- generator.py
+++ generator.py
@@ -7,7 +7,6 @@ import formulas
 import pdfkit
 
 spreadsheet = "/Users/libin/Downloads/sample_2.xlsx"
-# Provided column names remain consistent, Total Cost to Company is taken as input
 
 wb_init = xl.load_workbook(filename=spreadsheet, data_only=False)
 ws = wb_init.active
@@ -22,13 +21,9 @@ for i in range(num_Fixed):
     for cell in ws['A']:
         row_num += 1
         if cell.value == fixed_field:
-            print(ws['B' + str(row_num)].value)
             ws[columnL + str(row_num)] = fixed_value
-            print(ws[columnL + str(row_num)].value)
-            print(ws['B' + str(row_num)].value)
             break
-# input_CTC = 250000
-# field_name = "Base Salary"
+
 wb_init.save(spreadsheet)
 wb_init.close
 fpath = spreadsheet
@@ -38,9 +33,9 @@ xl_model.calculate()
 xl_model.write(dirpath=dirname)
 wb = xl.load_workbook(filename=spreadsheet, data_only=True)
 ws = wb.active
-# print(ws['B20'].value)
+
 wb.save(spreadsheet)
+
+#%%
 df = pd.read_excel(spreadsheet)
-# df.to_html("file.html")
-# pdfkit.from_file("file.html", "file.pdf")
 print(df)
