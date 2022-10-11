045ae56a3a77e17f0afba84f754ceaa08f74745d
albyjohnson <71396103+albyjohnson@users.noreply.github.com>
2022-10-11 08:07:12 +0530

    Armstrong Number
    Added a script to check whether a given number is Armstrong or not

---
+++ testArmstrong.py
@@ -0,0 +1,21 @@
+from tempfile import TemporaryDirectory
+
+
+def is_armstrong_number(number):
+    digCount = len(str(number))
+    # print(digCount)
+    tempNum = number
+    sum = 0
+    while tempNum > 0:
+        tempSum = int(tempNum % 10)
+        sum += tempSum**digCount
+        tempNum = int(tempNum/10)
+        # print(sum)
+        # print(tempNum)
+    if (number == sum):
+        return True
+    else:
+        return False
+
+
+print(is_armstrong_number(153))
