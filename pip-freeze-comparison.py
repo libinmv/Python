707a5d59fe8958e4961de73c3b76e5c856f4735c
Libin Mathew <loginlive10@gmail.com>
2022-11-02 11:41:03 +0530

    Update and rename pip-freeze-compare.py to pip-freeze-comparison.py

--- pip-freeze-comparison.py
+++ pip-freeze-comparison.py
@@ -1,6 +1,6 @@
 import re
-file1 = open("/Users/libinmathew/Downloads/prod_pip_freeze.txt", "r")
-file2 = open("/Users/libinmathew/Downloads/current.txt", "r")
+file1 = open("path_to_file1", "r")
+file2 = open("path to file2", "r")
 old = [str(re.sub("[0-9=.\n]", "", each)) for each in file1]
 new = [str(re.sub("[0-9=.\n]", "", each)) for each in file2]
 not_in = [package for package in old if not package in new]
